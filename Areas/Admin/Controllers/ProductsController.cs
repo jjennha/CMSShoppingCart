e6d378d027d822ab3b6d3d712d58a0abe05ee31e
Jenny Ha <ehs_jha@iastate.edu>
2020-01-29 16:44:02 -0600

    Product validation

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CmsShoppingCart.Infrastructure;
 using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -14,9 +16,11 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly CmsShoppingCartContext context;
-        public ProductsController(CmsShoppingCartContext context)
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public ProductsController(CmsShoppingCartContext context, IWebHostEnvironment webHostEnvironment)
         {
             this.context = context;
+            this.webHostEnvironment = webHostEnvironment;
         }
         // GET /admin/products
         public async Task<IActionResult> Index()
@@ -31,30 +35,43 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             return View();
         }
 
-        // POST /admin/pages/create
+        // POST /admin/products/create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Page page)
+        public async Task<IActionResult> Create(Product product)
         {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
             if (ModelState.IsValid)
             {
-                page.Slug = page.Title.ToLower().Replace(" ", "-");
-                page.Sorting = 100;
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
 
-                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                var slug = await context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
                 if (slug != null)
                 {
-                    ModelState.AddModelError("", "Page already exists");
-                    return View(page);
+                    ModelState.AddModelError("", "Product already exists");
+                    return View(product);
                 }
-                context.Add(page);
+                
+                string imageName = "noimage.png";
+                if (product.ImageUpload != null)
+                {
+                    string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + " " + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+                product.Image = imageName;
+
+                context.Add(product);
                 await context.SaveChangesAsync();
 
-                TempData["Success"] = "The page has been added!";
+                TempData["Success"] = "The product has been added!";
 
                 return RedirectToAction("Index");
             }
-            return View(page);
+            return View(product);
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,3 +13,13 @@
         },2000)
     }
 })
+
+function readUrl(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $("img#imgPreview").attr("src", e.target.result).width(200).height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
